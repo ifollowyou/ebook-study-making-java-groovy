13bc69805934b143e838415ab1c56c406ae1b881
ifollowyou <ifollowyou@aliyun.com>
2015-09-12 15:43:00 +0800

    update spring

--- ebook_source/ch07/AccountManagement/src/main/java/mjg/spring/dao/JdbcAccountDAO.groovy
+++ ebook_source/ch07/AccountManagement/src/main/java/mjg/spring/dao/JdbcAccountDAO.groovy
@@ -1,48 +1,32 @@
-/* ===================================================
- * Copyright 2012 Kousen IT, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ========================================================== */
 package mjg.spring.dao
 
-import java.sql.ResultSet
-import java.util.logging.Logger;
-
-import javax.sql.DataSource
-
 import mjg.spring.entities.Account
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
 
+import javax.sql.DataSource
+import java.sql.ResultSet
+import java.util.logging.Logger
+
 @Repository
 class JdbcAccountDAO implements AccountDAO {
-	Logger log = Logger.getLogger(JdbcAccountDAO.class.name)
+    Logger log = Logger.getLogger(JdbcAccountDAO.class.name)
 
     static int nextId = 3
     JdbcTemplate jdbcTemplate
 
-    @Autowired JdbcAccountDAO(DataSource dataSource) {
+    @Autowired
+    JdbcAccountDAO(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource)
     }
 
     public int createAccount(BigDecimal initialBalance) {
         String sql = "insert into accounts(id,balance) values(?,?)"
-		int id = nextId++
+        int id = nextId++
         int uc = jdbcTemplate.update(sql, id, initialBalance)
-		log.fine uc == 1 ? "account inserted with id=$id" : 'problem inserting account'
+        log.fine uc == 1 ? "account inserted with id=$id" : 'problem inserting account'
         return id;
     }
 
@@ -50,7 +34,7 @@ class JdbcAccountDAO implements AccountDAO {
         String sql = "select * from accounts where id=?"
         jdbcTemplate.queryForObject(sql, accountMapper, id)
     }
- 
+
 //    Java approach: inner class   
 //    class AccountMapper implements RowMapper<Account> {
 //        @Override
@@ -75,8 +59,8 @@ class JdbcAccountDAO implements AccountDAO {
     }
 
     def accountMapper = { ResultSet rs, int row ->
-        new Account(id: rs.getInt('id'), 
-            balance: rs.getBigDecimal('balance'))
+        new Account(id: rs.getInt('id'),
+                balance: rs.getBigDecimal('balance'))
     } as RowMapper<Account>
 }
 
--- ebook_source/ch07/AccountManagement/src/main/java/mjg/spring/services/AccountService.java
+++ ebook_source/ch07/AccountManagement/src/main/java/mjg/spring/services/AccountService.java
@@ -1,21 +1,5 @@
-/* ===================================================
- * Copyright 2012 Kousen IT, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ========================================================== */
-package mjg.spring.services;
 
-import java.math.BigDecimal;
+package mjg.spring.services;
 
 import mjg.spring.dao.AccountDAO;
 import mjg.spring.entities.Account;
@@ -23,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 @Transactional
